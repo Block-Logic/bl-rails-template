471d53d49f6176e56d6908dfd98c24cde3156d69
Klaudia Biala <klaudia.biala@polcode.net>
2023-08-03 21:14:35 +0200

    Tweak opt out form

--- app/controllers/opt_out_requests_controller.rb
+++ app/controllers/opt_out_requests_controller.rb
@@ -38,7 +38,7 @@ class OptOutRequestsController < ApplicationController
       ContactRequestMailer.new_opt_out_request_notice(@opt_out_request.id).deliver_now unless browser.bot?
       redirect_to thank_you_opt_out_requests_path
     else
-      render :new
+      render :new, status: :unprocessable_entity
     end
   end
 
