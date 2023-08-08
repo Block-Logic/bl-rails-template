0349e4ce7c7fc06b5b9f1e9bd6d5e36beb9ba9cb
maciejkocylapc <maciej.kocyla@polcode.net>
2023-08-08 10:10:22 +0200

    sign in with username

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,11 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+         :recoverable, :rememberable, :validatable, :confirmable,
+         authentication_keys: [:username]
+
+  validates :username,
+            presence: true,
+            uniqueness: { case_sensitive: false },
+            length: { minimum: 5, maximum: 40 }
 end
