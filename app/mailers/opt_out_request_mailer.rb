dc4de6f51f8f595dc41efc325abd1ee6850e0df3
Klaudia Biala <klaudia.biala@polcode.net>
2023-08-08 14:39:16 +0200

    Rename admin mailer

--- app/mailers/opt_out_request_mailer.rb
+++ app/mailers/opt_out_request_mailer.rb
@@ -1,4 +1,4 @@
-class AdminMailer < ApplicationMailer
+class OptOutRequestMailer < ApplicationMailer
   def new_opt_out_request_notice(request_id)
     @opt_out_request = OptOutRequest.find(request_id)
     mail(to: OPT_OUT_REQUESTS_MANAGER, subject: 'New opt-out request')
