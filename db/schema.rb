630a353c39e0fc0e13047ac58723ce4e8e626a81
Maciej Kocyła <maciej.kocyla@polcode.net>
2024-12-30 11:26:57 +0100

    update config files to rails v8

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2023_07_19_110230) do
+ActiveRecord::Schema[8.0].define(version: 2024_12_30_093256) do
   create_table "opt_out_requests", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.integer "request_type"
     t.json "meta_data"
