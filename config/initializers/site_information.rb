e93a8d7bf95a523a26c62d22a3eaae15b75411e1
Maciej Kocyła <maciej.kocyla@polcode.net>
2025-05-07 15:10:25 +0200

    update company address

--- config/initializers/site_information.rb
+++ config/initializers/site_information.rb
@@ -11,8 +11,8 @@ COMPANY_LLC = 'First Movers Advantage, LLC'
 OPT_OUT_REQUESTS_MANAGER = 'brian.long@fmadata.com'
 
 # Contact Info
-OUR_STREET_ADDRESS = '2575 Pearl St, Ste 230'
-OUR_CITY_STATE_ZIP = 'Boulder, CO 80302-3812'
+OUR_STREET_ADDRESS = '4845 Pearl East Cir Ste 118, PMB 417163'
+OUR_CITY_STATE_ZIP = 'Boulder, CO 80301'
 OUR_FAX = '(303) 443-2073'
 OUR_TELEPHONE = '(303) 443-2070'
 OUR_EMAIL_ADDRESS = 'contact@bl_rails_template.com'
